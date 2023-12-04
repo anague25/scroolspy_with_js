aff793a8e06e2c07460bbc66128600bce9a8a79b
Anague Sonna Roosvelt <anaguesonnaroosvelt@gmail.com>
2023-12-04 23:43:16 +0100

    test

--- scrollspy.js
+++ scrollspy.js
@@ -1,12 +1,28 @@
 /**
+ * @param {HTMLElement} elem
+ */
+const activate = function(elem){
+    const id = elem.getAttribute('id');
+    console.log(id);
+    // const anchor = document.querySelector(`a[href="#${id}"]`);
+}
+
+/**
  * 
  * @param {IntersectionObserverEntry} entries 
  * @param {IntersectionObserver} observer 
  */
 
 const callBack = function(entries,observer){
-console.log(entries);
+    entries.forEach((entry)=>{
+        if(entry.intersectionRatio > 0){
+            // activate(entry);
+          console.log(entry)
+        }
+    })
 }
+
+
 
 
 const spies = document.querySelectorAll('[data-spy]');
@@ -16,3 +32,7 @@ if(spies.length > 0){
         observer.observe(spy)
     })
 }
+
+const val = document.querySelector('.class1')
+console.log(val)
+console.log(val.getAttribute('id'))
